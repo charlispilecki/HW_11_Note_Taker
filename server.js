cf4fb31853b82f9469c21c5e5f39cfd3677ccce5
Charli <charlispilecki@gmail.com>
2021-01-03 20:48:14 -0600

    Added routes for html files and GET, POST and DELETE apis

--- server.js
+++ server.js
@@ -3,8 +3,9 @@
 const express = require("express");
 const { v4: uuidv4 } = require('uuid');
 const fs = require("fs");
-const dbJSON = require("./db.json");
 const path = require("path");
+
+let dbJSON = require("./db.json");
 
 // Sets up the Express App
 // =============================================================
@@ -15,26 +16,20 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Sets up the Express app to handle css and js files
+app.use(express.static('public'))
 
 
 // Routes
 // =============================================================
 
-// Basic route that sends the user first to the AJAX Page
-
-app.get("/", function(req, res) {
-  res.send("Proof of concept of storing data");
+// API to get all notes
+app.get("/api/notes", function(req, res) {
+  res.json(dbJSON)
 });
 
-app.get("/note", function(req, res) {
-  res.json(dbJSON);
-});
-
-app.post("/note", function(req, res) {
-  // Validate request body
-  if(!req.body.title) {
-    return res.json({error: "Missing required title"});
-  }
+// API to add a note
+app.post("/api/notes", function(req, res) {
 
   // Copy request body and generate ID
   const note = {...req.body, id: uuidv4()}
@@ -42,21 +37,44 @@ app.post("/note", function(req, res) {
   // Push note to dbJSON array - saves data in memory
   dbJSON.push(note);
 
-  // Saves data to file by persisting in memory variable dbJSON to db.json file.
-  // This is needed because when we turn off server we loose all memory data like pbJSON variable.
-  // Saving to file allows us to read previous notes (before server was shutdown) from file.
+  // Saves data to file
   fs.writeFile(path.join(__dirname, "db.json"), JSON.stringify(dbJSON), (err) => {
     if (err) {
       return res.json({error: "Error writing to file"});
     }
-
     return res.json(note);
-  });
+  })
+    
 });
 
-app.get("*", function(req, res) {
-  res.send("Sending you the homepage");
+// API to delete a note
+app.delete("/api/notes/:id", function(req, res) {
+
+  // Filter out the id that needs to be deleted
+  dbJSON = dbJSON.filter(note => {
+    return note.id !== req.params.id
+  })
+
+  // Saves data to file
+  fs.writeFile(path.join(__dirname, "db.json"), JSON.stringify(dbJSON), (err) => {
+    if (err) {
+      return res.json({error: "Error writing to file"});
+    }
+    return res.send();
+  })
+
 });
+
+// Notes page
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// Home page
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
 
 // Starts the server to begin listening
 // =============================================================
